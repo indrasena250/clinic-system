17f9bb895ff3d4b53d599d734042da91f91df983
indrasena250 <indrasenareddy787@gmail.com>
2026-04-09 00:10:09 +0530

    server fixed

--- backend/server.js
+++ backend/server.js
@@ -166,7 +166,10 @@ app.use((err, req, res, next) => {
   console.error("🔥 EXPRESS ERROR:", err);
   res.status(500).json({ message: "Internal Server Error" });
 });
-
+app.use((req, res, next) => {
+  console.log("🌐 Incoming:", req.method, req.url);
+  next();
+});
 // ===============================
 // START SERVER
 // ===============================
