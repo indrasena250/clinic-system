15dc7afeae12d6b6c5a3f17c77c6085a2bc73243
indrasena250 <indrasenareddy787@gmail.com>
2026-04-09 00:14:21 +0530

    server fixed

--- backend/server.js
+++ backend/server.js
@@ -175,6 +175,6 @@ app.use((req, res, next) => {
 // ===============================
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
